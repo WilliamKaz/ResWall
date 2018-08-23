69ced88f45f3c7e28b7646025aef9119466b542c
Jordan Youngs <jordankyoungs@gmail.com>
2018-08-23 01:04:35 +0000

    setup a script file that I will use to test out queries

---
+++ knex_script.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const knex = require('knex')({
+  client: 'postgresql',
+  connection: {
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+    port     : process.env.DB_PORT,
+    ssl      : process.env.DB_SSL
+  },
+});
+
+knex.select().from('users').then(function(result){console.log(result)});
