23f8697df9c3f681d563838defb2ceb9031baf00
Jordan Youngs <jordankyoungs@gmail.com>
2018-08-23 21:32:03 +0000

    changed functions to use async/await so I can actually return values

--- knex_script.js
+++ knex_script.js
@@ -13,44 +13,90 @@ const knex = require('knex')({
     ssl      : process.env.DB_SSL
   },
 });
+const test = async function () {
+  return await
+  knex.select().from('resources')
+  .where('user_id', 1);
+
+};
+
+(async function() {
+  console.log(await test());
+}());
+
+
+// async function getProfile2(user_id) {
+//   return await knex.select('username', 'bio').from('users')
+//   .where('id', user_id)
+//   .then((result) => {return (result[0])})
+// }
+
+// (async function() {
+//   const p = await getProfile2(2);
+//   console.log(p);
+// }())
+// .then(()=>{});
+// const createUser = async (username, password, email, bio) => {
+//   return await knex('users').insert({
+//     username,
+//     password,
+//     email,
+//     bio
+//   })
+// }
+
+// (async function() {
+//   const p = await createUser('bordan', '123', 'bordan@email.com', 'this is a bio');
+//   console.log(p);
+// }())
+
 
 
 module.exports = function makeDataHelpers(knex) {
   return {
 
-    createUser: (username, password, email, bio) => {
+    createUser: async (username, password, email, bio) => {
+      return await
       knex('users').insert({
         username,
         password,
         email,
         bio
-      }).then(()=>{});
+      });
     },
 
-    checkCredentials: (email, password) => {
+    checkCredentials: async (email, password) => {
+      return await
       knex('users').count('email')
       .where('email', email)
-      .then((result) => {return (result[0].count === '1')})
-      .catch((err)=>{console.log(err)});
+      .then((result) => {return (result[0].count === '1')});
     },
 
-    getUserId: (email) => {
+    getUserId: async (email) => {
+      return await
       knex.select('id').from('users')
       .where('email', email)
-      .then((result)=>{return (result[0].id)})
-      .catch((err)=>{console.log(err)});
+      .then((result) => {return (result[0].id)})
     },
 
-    saveTweet: function(newTweet, callback) {
-      db.collection("tweets").insertOne(newTweet, (err) => {
-        callback(err);
-      });
+    getProfile: async (user_id) => {
+      return await
+      knex.select('username', 'bio').from('users')
+      .where('id', user_id)
+      .then((result) => {return (result[0])});
     },
 
-    saveTweet: function(newTweet, callback) {
-      db.collection("tweets").insertOne(newTweet, (err) => {
-        callback(err);
-      });
+    updateProfile: async (user_id, bio) => {
+      return await
+      knex('users')
+      .where('id', user_id)
+      .update({bio});
+    },
+
+    getMyResources: async (user_id) => {
+      return await
+      knex.select().from('resources')
+      .where('user_id', user_id);  ///unsorted still
     },
 
     getTweets: function(callback) {
