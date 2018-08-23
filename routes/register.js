5abe9948d647da2cfce492c441939228a9b1b0f3
Chris Wang <chriswangscm@gmail.com>
2018-08-23 15:56:52 +0000

    updated registration page routing

--- routes/register.js
+++ routes/register.js
@@ -2,24 +2,29 @@
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 module.exports = () => {
 
   // render the registration page
   router.get("/register", (req, res) => {
-    res.render('register');
+    res.render('/register');
   });
 
   // submit registration form
+  // add user information to database, set cookie, redirect to home page
   router.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = bcrypt.hashSync(req.body.password, 10);
+    const email = req.body.email;
+    const bio = req.body.bio;
 
-    // add user information to database, set cookie, redirect to resources page
-    // db.users.username = req.body.username;
-    // db.users.password = bcrypt.hashSync(req.body.password, 10);
-    // db.users.email = req.body.email;
-    // db.users.bio = req.body.bio;
-    // db.req.session.user_id = TBD;
-    // db.res.redirect("/resources/");
+    // create user in db and get the user id
+    createUser(username, password, email, bio);
+    const userId = getUserId(email);
+
+    req.session.userId = userId;
+    res.redirect("/index");
   });
 
   return router;
