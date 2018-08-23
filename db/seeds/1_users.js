f36ebacfb86df9801c56d312144a133ace1a564c
Jordan Youngs <jordankyoungs@gmail.com>
2018-08-23 00:18:30 +0000

    Had to make a new seed file that deletes tables in a specific order to avoid foreign key errors(you are not allowed to delete a key that is referenced by another table)

--- db/seeds/1_users.js
+++ db/seeds/1_users.js
@@ -1,29 +1,26 @@
 
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
-      return Promise.all([
-        knex('users').insert({
-          id: 1,
-          username: 'Alice',
-          password: 'asdf',
-          email: 'alice@email.com',
-          bio: 'Hello, my name is Alice!'
-        }),
-        knex('users').insert({
-          id: 2,
-          username: 'Bob',
-          password: 'asdf',
-          email: 'bob@email.com',
-          bio: 'Hello, my name is Bob!'
-        }),
-        knex('users').insert({
-          id: 3,
-          username: 'Charlie',
-          password: 'asdf',
-          email: 'charlie@email.com',
-          bio: 'Hello, my name is Charlie!'
-        })
-      ]);
-    });
+  return Promise.all([
+    knex('users').insert({
+      id: 1,
+      username: 'Alice',
+      password: 'asdf',
+      email: 'alice@email.com',
+      bio: 'Hello, my name is Alice!'
+    }),
+    knex('users').insert({
+      id: 2,
+      username: 'Bob',
+      password: 'asdf',
+      email: 'bob@email.com',
+      bio: 'Hello, my name is Bob!'
+    }),
+    knex('users').insert({
+      id: 3,
+      username: 'Charlie',
+      password: 'asdf',
+      email: 'charlie@email.com',
+      bio: 'Hello, my name is Charlie!'
+    })
+  ]);
 };
