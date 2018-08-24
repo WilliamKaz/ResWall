084ea2a461523fe1e3e6d652f16766b1ab5d5ee9
Jordan Youngs <jordankyoungs@gmail.com>
2018-08-24 02:29:46 +0000

    Made a migration to give resources an average_rating column, adjusted seed files to include this

--- db/seeds/3_resources.js
+++ db/seeds/3_resources.js
@@ -7,7 +7,8 @@ exports.seed = function(knex, Promise) {
       title: 'Music Resource',
       description: 'A site of tutorials where you can learn about music.',
       user_id: 3,
-      topic_id: 1
+      topic_id: 1,
+      average_rating: 3.5
     }),
     knex('resources').insert({
       id: 2,
@@ -15,7 +16,8 @@ exports.seed = function(knex, Promise) {
       title: 'Health and Wellness blog',
       description: 'A blog about health and wellness',
       user_id: 1,
-      topic_id: 4
+      topic_id: 4,
+      average_rating: 5
     }),
     knex('resources').insert({
       id: 3,
@@ -23,7 +25,8 @@ exports.seed = function(knex, Promise) {
       title: 'Writer\'s Blog',
       description: 'A blog all about writing.',
       user_id: 2,
-      topic_id: 2
+      topic_id: 2,
+      average_rating: 4.5
     }),
     knex('resources').insert({
       id: 4,
@@ -31,7 +34,8 @@ exports.seed = function(knex, Promise) {
       title: 'Engineering 101',
       description: 'A video about the fundamentals of engineering.',
       user_id: 1,
-      topic_id: 3
+      topic_id: 3,
+      average_rating: 1.5
     }),
     knex.raw('ALTER SEQUENCE resources_id_seq RESTART WITH 5')
   ]);
