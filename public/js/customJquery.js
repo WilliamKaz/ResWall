5cff2ed484ebc54e8bbf1b4db55b8d8d908221ce
WilliamKasurak <willkdesign@gmail.com>
2018-08-24 23:06:34 +0000

    fuk u jquery

--- public/js/customJquery.js
+++ public/js/customJquery.js
@@ -1,37 +1,25 @@
-$(function() {
+$('body').on('click','.show', function() {
+  $(this).closest('.post').find('.hide').toggle();
+  $(this).closest('.post').find('.show').toggle();
+  $(this).closest('.post').find(".commentContainer").slideDown();
+});
+$('body').on('click','.hide', function() {
+  $(this).closest('.post').find('.show').toggle();
+  $(this).closest('.post').find('.hide').toggle();
+  $(this).closest('.post').find(".commentContainer").slideUp();
+});
 
 
 
-
-
-}
-);
-
-
-  $('.show').on('click', function() {
-    $(this).closest('.post').find('.hide').toggle();
-    $(this).closest('.post').find('.show').toggle();
-    $(this).closest('.post').find(".commentContainer").slideDown();
-  });
-
-   $('.hide').on('click', function() {
-    $(this).closest('.post').find('.show').toggle();
-    $(this).closest('.post').find('.hide').toggle();
-    $(this).closest('.post').find(".commentContainer").slideUp();
-  });
-
-  $('.commentSubmit').on('click', function(e) {
-     e.preventDefault();
-    $('.commentInput').val('');
-  });
-
-
-
+$('body').on('click','.commentSubmit', function(e) {
+  // e.preventDefault();
+  $('.commentInput').val('');
+});
 
    // code sourced from https://codepen.io/neilpomerleau/pen/wzxzQr
    // logs status of star rating
-  $(':radio').change(function() {
-    console.log('New star rating: ' + this.value);
-  });
+$('body').on('change',':radio',(function() {
+  console.log('New star rating: ' + this.value);
+});
 
 
