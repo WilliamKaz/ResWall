279eb6916feac469116b09ca48f184127c59ac01
WilliamKasurak <willkdesign@gmail.com>
2018-08-24 21:56:46 +0000

    fixing css

--- public/js/customJquery.js
+++ public/js/customJquery.js
@@ -7,19 +7,28 @@ $(function() {
     $(this).closest('.post').find('.hide').toggle();
     $(this).closest('.post').find('.show').toggle();
     $(this).closest('.post').find(".commentContainer").slideDown();
-});
+  });
 
    $('.hide').on('click', function() {
     $(this).closest('.post').find('.show').toggle();
     $(this).closest('.post').find('.hide').toggle();
     $(this).closest('.post').find(".commentContainer").slideUp();
-});
+  });
+
+  $('.commentSubmit').on('click', function(e) {
+     e.preventDefault();
+    $('.commentInput').val('');
+  });
+
+
+
 
    // code sourced from https://codepen.io/neilpomerleau/pen/wzxzQr
    // logs status of star rating
   $(':radio').change(function() {
     console.log('New star rating: ' + this.value);
   });
+
 
 
 
