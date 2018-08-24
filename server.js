a4c64196fbd23799b44e563272a01935fc9d0f4d
Chris Wang <chriswangscm@gmail.com>
2018-08-24 14:48:30 +0000

    updated server js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// require('dotenv').config();
+require('dotenv').config();
 
 const PORT        = process.env.PORT || 8080;
 // const ENV         = process.env.ENV || "development";
@@ -11,10 +11,10 @@ const cookieSession = require('cookie-session');
 // const sass        = require("node-sass-middleware");
 const app         = express();
 
-// const knexConfig  = require("./knexfile");
-// const knex        = require("knex")(knexConfig[ENV]);
-// const morgan      = require('morgan');
-// const knexLogger  = require('knex-logger');
+const knexConfig  = require("./knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+const morgan      = require('morgan');
+const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const loginRoutes = require("./routes/login");
@@ -26,10 +26,10 @@ const usersRoutes = require("./routes/users");
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
-// app.use(knexLogger(knex));
+app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
